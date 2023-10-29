e9b8e0ecf598e4b77c0fbb2e6331fdc06a002116
Mehdizaiem <mehdizaiem@hotmail.fr>
2023-10-29 15:21:26 +0100

    Comitpls

---
+++ src/Form/BookType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ref')
+            ->add('title')
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'Science Fiction' => 'Science Fiction',
+                    'Mystery' => 'Mystery',
+                    'Autobiography' => 'Autobiography',
+                ],
+                'placeholder' => 'Select a category'    
+            ])
+            ->add('publicationDate')
+            ->add('published',CheckboxType::class, ['data' => true])
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
